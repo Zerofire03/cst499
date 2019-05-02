ca13c052c318a84dda847b164bab8a8ab90a93d9
Christopher Holmes <zerofire.3@gmail.com>
2019-05-02 02:57:33 +0000

    Changed username and email

--- VolunteerSkillShare/loginProcess.php
+++ VolunteerSkillShare/loginProcess.php
@@ -19,46 +19,13 @@
  
     // pass value to the command
     $stmt->bindParam(':id', $customerNumber, PDO::PARAM_INT);
- 
-    // execute the stored procedure
-    $stmt->execute();
-    
-    //Login with admin username querying admin db
-    if($username == "admin")
-    {
-        $sql = "SELECT * FROM cat_admin where username = :username AND password = :password";
-    }
-    
-    //Regular user login
-    else
-    {
-        $sql = "SELECT * FROM cat_user where email = :username AND password = :password";
-    }
-    //integrate this with database
-    //$sql = "SELECT * FROM cat_admin where username = :username AND password = :password";
     
     $np = array();
     $np[":username"] = $username;
     $np[":password"] = $password;
-    echo $sql;
-    $stmt = $conn->prepare($sql);
-    $stmt->execute($np);
-    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+    // execute the stored procedure
+    $stmt->execute();
     
-    if (empty($record)){
-        $_SESSION['incorrect'] = true;
-        header("Location:login.php");
-    }
-    //elseif($_SESSION['incorrect'] = false && $_SESSION['adminName'] == $record['firstName'] . " " > $record['lastName'])
-    elseif($username == "admin"){
-        $_SESSION['incorrect'] = false;
-        $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName']; 
-        header("Location:admin.php");
-    }
-    else{
-        $_SESSION['incorrect'] = false;
-        $_SESSION['userName'] = $record['firstName'] . " " . $record['lastName'];
-        $_SESSION['userId'] = $record['userId'];
-        header("Location:menu.php");
-    }
+    
 ?>
