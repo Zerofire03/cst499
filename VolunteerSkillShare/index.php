ceea72bee84cd03203fb2916b27ef5c31bb0b317
EC2 Default User <ec2-user@ip-172-31-35-68.us-east-2.compute.internal>
2019-05-03 05:07:15 +0000

    Added page navigation with files and css styling to all pages.

--- VolunteerSkillShare/index.php
+++ VolunteerSkillShare/index.php
@@ -12,10 +12,24 @@
         <title>Volunteer Skill Share</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!--<style>@import url("css/styles.css");</style>-->
+        <style>@import url("css/styles.css");</style>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates" rel="stylesheet">
     </head>
     
-    <body>
+    <body id="activePage">
+        
+        <div class="jumbotron text-center">
+        <h1>VOLUNTEER SKILL SHARE</h1>
+        </div>
+        
+        <!-- Navigation Bar-->
+        <ul class="nav nav-pills">
+          <li role="presentation" class="active"><a href="index.php">Login</a></li>
+          <li role="presentation"><a href="volProfile.php">Volunteer Profile</a></li>
+          <li role="presentation"><a href="orgProfile.php">Organization Profile</a></li>
+          <li role="presentation"><a href="orgSearch.php">Active Project Search</a></li>
+        </ul>
+        
        <!--Login Process-->
         <div class="instructions">
         <h3><font color="black">Please Login</font></h3>
@@ -43,6 +57,20 @@
         </form>
         </div>
         </div>
+        
+        <!-- This is the footer -->
+        <footer>
+            <hr id="hr_footer">
+                CST 499 Group Capstone. 2019&copy; Buckey, Gonzalez, Holmes, Loeser<br />
+                <strong>Disclaimer:</strong> The information in this webpage is fictious.<br />
+                It is used for academic purposes only.
+                
+                <figure id="csumb">
+                    <img src="Images/csumb_logo.png" alt="CSUMB Logo">
+                </figure>
+            
+        </footer>
+        <!-- closing footer -->
     </body>
     
 </html>
---
+++ VolunteerSkillShare/volProfileEdit.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Volunteer Skill Share</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <style>@import url("css/styles.css");</style>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates" rel="stylesheet">
+    </head>
+    
+    <body id="activePage">
+        
+        <div class="jumbotron text-center">
+        <h1>VOLUNTEER SKILL SHARE</h1><br/>
+        <h2>Welcome <?=$_SESSION['userName']?>!</h2><br/>
+        </div>
+        
+        <!-- Navigation Bar-->
+        <ul class="nav nav-pills">
+          <li role="presentation"><a href="index.php">Home</a></li>
+          <li role="presentation"><a href="volProfile.php">Volunteer Profile</a></li>
+          <li role="presentation" class="active"><a href="volPRofileEdit.php">Profile Editor</a></li>
+          <li role="presentation"><a href="orgSearch.php">Active Project Search</a></li>
+        </ul>
+        
+       <!-- code -->
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        
+        <!-- This is the footer -->
+        <footer>
+            <hr id="hr_footer">
+                CST 499 Group Capstone. 2019&copy; Buckey, Gonzalez, Holmes, Loeser<br />
+                <strong>Disclaimer:</strong> The information in this webpage is fictious.<br />
+                It is used for academic purposes only.
+                
+                <figure id="csumb">
+                    <img src="Images/csumb_logo.png" alt="CSUMB Logo">
+                </figure>
+            
+        </footer>
+        <!-- closing footer -->
+    </body>
+    
+</html>
