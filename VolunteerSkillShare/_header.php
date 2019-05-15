5a2bf9f34dc627671cc2c130823dfe0a760a9204
Patrick Gonzalez <patrickgonalez@live.com>
2019-05-15 02:07:59 +0000

    More css styling for nav bar and drop down for logins

--- VolunteerSkillShare/_header.php
+++ VolunteerSkillShare/_header.php
@@ -10,5 +10,5 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <style>@import url("css/styles.css");</style>
-        <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
     </head>
--- VolunteerSkillShare/volProfile.php
+++ VolunteerSkillShare/volProfile.php
@@ -1,6 +1,6 @@
 <?php 
     include '_header.php';
-    include '_enforceLogin.php';
+    //include '_enforceLogin.php';
 ?>
 
 
@@ -23,29 +23,6 @@
        <!-- code -->
        
        
-       
-       
-       
-       
-       
-       
-       
-       
-       
         
         <!-- This is the footer -->
-        <footer>
-            <hr id="hr_footer">
-                CST 499 Group Capstone. 2019&copy; Buckey, Gonzalez, Holmes, Loeser<br />
-                <strong>Disclaimer:</strong> The information in this webpage is fictious.<br />
-                It is used for academic purposes only.
-                
-                <figure id="csumb">
-                    <img src="Images/csumb_logo.png" alt="CSUMB Logo">
-                </figure>
-            
-        </footer>
-        <!-- closing footer -->
-    </body>
-    
-</html>
+        <?php include '_footer.php';
