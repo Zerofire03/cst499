e6a3d536b2ad5960c6662c68235449621c3a78bd
Christopher Holmes <zerofire.3@gmail.com>
2019-05-19 18:57:10 +0000

    Added navBar.php and logout button

---
+++ VolunteerSkillShare/_navBar.php
@@ -0,0 +1,8 @@
+ <!-- Navigation Bar-->
+        <ul class="nav nav-pills" id="navBar">
+          <li role="presentation" class="active"><a href="index.php">Login</a></li>
+          <li role="presentation"><a href="volProfile.php">Volunteer Profile</a></li> 
+          <li role="presentation"><a href="orgProfile.php">Organization Profile</a></li>
+          <li role="presentation"><a href="orgSearch.php">Active Project Search</a></li>
+          <li role="presentation"><a href="logout.php">Logout</a></li>
+        </ul>
